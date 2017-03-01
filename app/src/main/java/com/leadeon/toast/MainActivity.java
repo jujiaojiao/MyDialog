46e7a867a707c6c6f0106419d93a261b54b3e527
WIN7-20161121ZY\Administrator <gino1993>
2017-03-01 15:00:22 +0800

    Initial commit

---
+++ app/src/main/java/com/leadeon/toast/MainActivity.java
@@ -0,0 +1,27 @@
+package com.leadeon.toast;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.leadeon.toast.utils.MyDialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        MyDialog instance = MyDialog.getInstance();
+        instance.showDialog(this,Color.RED,R.layout.toast);
+    }
+}
